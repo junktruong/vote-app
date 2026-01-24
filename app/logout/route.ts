59dfbd6f4ecaed9fa09982742d5529f46204b2b4
junktruong <junk.truongg@gmail.com>
2026-01-24 10:42:37 +0700

    fix

--- app/logout/route.ts
+++ app/logout/route.ts
@@ -3,6 +3,6 @@ import { clearUserSession } from "@/lib/auth";
 
 export async function GET() {
   await clearUserSession();
-  return NextResponse.redirect(new URL("/", "http://localhost:3000"));
+  return NextResponse.redirect(new URL("/"));
 }
     
